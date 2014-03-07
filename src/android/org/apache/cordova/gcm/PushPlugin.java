8b128870c97b29be7afefe7e2fd86c24b3942c63
Gwenael YAP <gyap@ymagyn.com>
2014-03-07 10:48:03 +0100

    ajout d'un delay sur le renvoi de de l'object JSON dans le javascript

--- src/android/org/apache/cordova/gcm/PushPlugin.java
+++ src/android/org/apache/cordova/gcm/PushPlugin.java
@@ -100,12 +100,17 @@ public class PushPlugin extends CordovaPlugin {
 	 * Sends a json object to the client as parameter to a method which is defined in gECB.
 	 */
 	public static void sendJavascript(JSONObject _json) {
-		String _d = "javascript:" + gECB + "(" + _json.toString() + ")";
-		Log.v(TAG, "sendJavascript: " + _d);
+		final JSONObject t = _json;
+		new android.os.Handler().postDelayed(new Runnable(){
+			public void run(){
+				String _d = "javascript:" + gECB + "(" + t.toString() + ")";
+				Log.v(TAG, "sendJavascript: " + _d);
 
-		if (gECB != null && gWebView != null) {
-			gWebView.sendJavascript(_d); 
-		}
+				if (gECB != null && gWebView != null) {
+					gWebView.sendJavascript(_d); 
+				}
+			}
+		}, 1500);
 	}
 
 	/*
